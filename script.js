3a2d6bbf8b55a41f0ba2cfd09ae3a0c32474eeeb
Aaron Zheng <153031902+aaronxyzheng@users.noreply.github.com>
2025-11-11 09:16:39 -0500

    feat: add basic UI
    Note: needs styling

--- script.js
+++ script.js
@@ -15,21 +15,32 @@ function getComputerChoice() {
 
 
 function playRound(humanChoice, computerChoice) {
+    
+    if (gameOver === true) return;
 
     humanChoice = humanChoice.toLowerCase();
+    const resultDiv = document.querySelector("#result-div");
+    const scoreDiv = document.querySelector("#score-div");
+    const gameResultDiv = document.querySelector("#game-result-div");
 
     if(humanChoice === computerChoice) {
-        console.log("Tie! You guys both chose: " + humanChoice);
+        resultDiv.textContent = "Tie! You guys both chose: " + humanChoice;
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "scissors" && computerChoice === "paper") ||
         (humanChoice === "paper" && computerChoice === "rock")
     ) {
-        console.log("You won! You chose: " + humanChoice + ", Computer Chose: " + computerChoice);
+        resultDiv.textContent = "You won! You chose: " + humanChoice + ", Computer Chose: " + computerChoice;
         humanScore += 1;
     } else {
-        console.log("You lost! You chose: " + humanChoice + ", Computer Chose: " + computerChoice);
+        resultDiv.textContent = "You lost! You chose: " + humanChoice + ", Computer Chose: " + computerChoice;
         computerScore += 1;
+    }
+
+    scoreDiv.textContent = "Human Score: " + humanScore + "  Computer Score: " + computerScore;
+    if(humanScore >= 5 || computerScore >= 5) {
+        humanScore > computerScore ? gameResultDiv.textContent = "Game Over. You Won!" : gameResultDiv.textContent = "Game Over. Computer Won.";
+        gameOver = true;
     }
 }
 
@@ -42,5 +53,7 @@ for (const button of buttonList) {
     })
 }
 
+
 let humanScore = 0;
+let gameOver = false;
 let computerScore = 0;
